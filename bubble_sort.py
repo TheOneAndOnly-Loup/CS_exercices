e0991e7355066653aef30788664a87b4707e1d01
TheOneAndOnly-Loup <loupcorbeilpintoduraes@gmail.com>
2026-09-24 14:29:58 +0200

    bubble_sort

---
+++ bubble_sort.py
@@ -0,0 +1,10 @@
+def bubbleSort(array):
+    for i in range(len(array)):
+        for j in range(0, len(array) - i - 1):
+            if array[j] > array[j+1]:
+                array[j], array[j+1] = array[j+1], array[j]
+    return array
+
+data = [-2, 45, 0, 11, -9]
+
+print(bubbleSort(data))
